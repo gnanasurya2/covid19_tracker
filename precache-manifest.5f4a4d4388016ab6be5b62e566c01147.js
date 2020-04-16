befb70f1f1b7b8bb867a1a596f2c342d90a81442
gnanasurya <gnanasurya2@gmail.com>
2020-04-16 22:55:30 +0530

    Updates

---
+++ precache-manifest.5f4a4d4388016ab6be5b62e566c01147.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125a4a1348436c904ab996d8cc7f1d78",
+    "url": "/covid19_tracker/index.html"
+  },
+  {
+    "revision": "6d8407b64ebbc0c1beab",
+    "url": "/covid19_tracker/static/css/main.301a08d5.chunk.css"
+  },
+  {
+    "revision": "e9f0ee7de48290f33a3b",
+    "url": "/covid19_tracker/static/js/2.d77d0a96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19_tracker/static/js/2.d77d0a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8407b64ebbc0c1beab",
+    "url": "/covid19_tracker/static/js/main.8c0b1aa3.chunk.js"
+  },
+  {
+    "revision": "2183b9a547aec37d1c65",
+    "url": "/covid19_tracker/static/js/runtime-main.544c4f78.js"
+  },
+  {
+    "revision": "a33d470812d262df4391af2df8ce36e2",
+    "url": "/covid19_tracker/static/media/logo.a33d4708.png"
+  }
+]);
